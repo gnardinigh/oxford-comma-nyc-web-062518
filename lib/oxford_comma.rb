21a3aa50dfa098ade9fa0cc8eba0a091666dc5db
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 23:55:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,21 +1,5 @@
 
+a = ["a","b","c","d"]
 
 
 
-
-
-def oxford_comma(array)
-  if array.size==1
-    return array.join
-  elsif array.size>1
-  
-  final_element=array.pop()
-  array.join(" ")
-  
-  
-  
-  
-  
-  
-end
-end
