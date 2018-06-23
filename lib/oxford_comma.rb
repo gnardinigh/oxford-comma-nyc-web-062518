dfda859d35f925d156a793051fec0a7af0bac9ad
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 23:58:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,12 @@
 
 
 def oxford_comma(a)
+  if a.size==1
+    return a.join
 
-final_element = a.pop
-a_string = a.join(", ")
-a_string+=" and #{final_element}"
-puts a_string
+  final_element = a.pop
+  a_string = a.join(", ")
+  a_string+=" and #{final_element}"
+
 
 end
