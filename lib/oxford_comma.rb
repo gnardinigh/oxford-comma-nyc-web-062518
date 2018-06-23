0d8e3a77fa6610e79fa7223aaed10a96920e017b
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 23:55:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,21 @@
 
 
 
-def oxford comma
+
+
+
+def oxford_comma(array)
+  if array.size==1
+    return array.join
+  elsif array.size>1
   
+  final_element=array.pop()
+  array.join(" ")
+  
+  
+  
+  
+  
+  
+end
 end
