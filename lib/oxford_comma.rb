7f539dd29e8d299958198da1d3159336b839c2c8
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 23:44:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,6 @@
 def oxford_comma(array)
-  array.join
-end
-
-def oxford_comma(array)
-  array.join(" and" )
-end
+  if array.size==1
+    array.join
+  elsif array.size==2
+    array.join(" and ")
+  
