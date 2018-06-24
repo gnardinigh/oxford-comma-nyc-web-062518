8f30568a42af09fe674e43cc9d1fbfde476d9a7d
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 00:00:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(a)
   if a.size==1
     return a.join
   elsif a.size==2
-    return a.join(", ")
+    return a.join(" and ")
   else
   final_element = a.pop
   a_string = a.join(", ")
